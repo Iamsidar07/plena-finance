442bfa7830a022f0fe6f1507923dc19ff24dd546
iamsidar07 <ms8460149@gmail.com>
2023-12-28 19:24:24 +0530

    fetched product detail

--- src/screens/ProductDetailScreen.jsx
+++ src/screens/ProductDetailScreen.jsx
@@ -1,81 +1,36 @@
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
+import { showToast } from "../utils/toast";
+import { BASE_URL } from "../config";
+import ProductItem from "../components/ProductItem";
 
-export default function ProductDetailScreen() {
+export default function ProductDetailScreen({ route, navigation }) {
+    const { id } = route.params;
+    const [product, setProduct] = useState({});
+    const [isFetchingProduct, setIsFetchingProduct] = useState(true);
+    const [isError, setIsError] = useState(false);
+
+    useEffect(() => {
+        const getProductDetails = async () => {
+            try {
+                setIsError(false);
+                setIsFetchingProduct(true);
+                const response = await fetch(`${BASE_URL}/products/${id}`);
+                const data = await response.json();
+                console.log(data);
+                setProduct(data);
+            } catch (error) {
+                showToast(error.message);
+                setIsError(true);
+            } finally {
+                setIsFetchingProduct(false);
+            }
+        };
+        getProductDetails();
+    }, [])
     return (
-        // <View style={styles.container}>
-        //     <SafeAreaView style={styles.headerContainer}>
-        //         <View style={styles.headerTitleContainer}>
-        //             <Text style={styles.headerTitle}>Hey, Rahul</Text>
-        //             <Pressable>
-        //                 <Image source={cartIcon} style={styles.cartIcon} />
-        //             </Pressable>
-        //         </View>
-        //         <View style={styles.inputContainer}>
-        //             <Image source={searchIcon} style={styles.searchIcon} />
-        //             <TextInput
-        //                 style={styles.input}
-        //                 placeholder="Search for products or store"
-        //                 placeholderTextColor={"#8891A5"}
-        //                 cursorColor={"#8891A5"}
-        //             />
-        //         </View>
-        //         <View style={styles.deliveryDetailContainer}>
-        //             {/* left */}
-        //             <View style={styles.deliveryDetailLeftContainer}>
-        //                 <Text style={styles.deliveryDetailTitle}>
-        //                     DELEVERY TO
-        //                 </Text>
-        //                 <View style={styles.deliveryDetailDescriptionContainer}>
-        //                     <Text style={styles.deliveryDetailDescription}>
-        //                         {location.length >= 20 ? `${location.slice(0, 20)}...` : location}
-        //                     </Text>
-        //                     <Pressable>
-        //                         <Image source={arrowIcon} style={styles.arrowIcon} />
-        //                     </Pressable>
-        //                 </View>
-        //             </View>
-        //             {/* right */}
-        //             <View style={styles.deliveryDetailLeftContainer}>
-        //                 <Text style={styles.deliveryDetailTitle}>
-        //                     WITHIN
-        //                 </Text>
-        //                 <View style={styles.deliveryDetailDescriptionContainer}>
-        //                     <Text style={styles.deliveryDetailDescription}>
-        //                         1 HOUR
-        //                     </Text>
-        //                     <Pressable>
-        //                         <Image source={arrowIcon} style={styles.arrowIcon} />
-        //                     </Pressable>
-        //                 </View>
-        //             </View>
-        //         </View>
-        //     </SafeAreaView>
-        //     <View>
-        //         <FlatList
-        //             data={data}
-        //             renderItem={carouselItem}
-        //             keyExtractor={(item) => item.id.toString()}
-        //             horizontal
-        //             contentContainerStyle={{
-        //                 padding: 10,
-        //             }}
-        //         />
-        //     </View>
-        //     <Text style={styles.recommendedTitle}>Recommended</Text>
-        //     <>
-        //         {
-        //             isFetchingProducts ? <Text>Loading...</Text> : <FlatList
-        //                 data={products}
-        //                 renderItem={renderProductItem}
-        //                 keyExtractor={(item) => item.id.toString()}
-        //                 numColumns={2}
-        //                 contentContainerStyle={{
-        //                     padding: 10,
-        //                 }}
-        //             />
-        //         }
-        //     </>
-        // </View>
-        <></>
+        <>
+            <Text>{product?.title}</Text>
+        </>
     )
 }
