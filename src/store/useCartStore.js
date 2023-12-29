097becc9371ab7088dfb8ff52e641d1fc987ef0a
iamsidar07 <ms8460149@gmail.com>
2023-12-29 12:37:58 +0530

    added increase and decrease quantity

--- src/store/useCartStore.js
+++ src/store/useCartStore.js
@@ -33,6 +33,29 @@ const useCartStore = create((set) => ({
             return { cart: updatedCart };
         }
     }),
+    increaseQuantity: (id) => set((state) => {
+        const product = state.cart.find((item) => item.id === id);
+        if (!product) {
+            showToast("No product found.");
+        };
+        product["quantity"] += 1;
+        const updatedCart = [...state.cart, product];
+        console.log(updatedCart);
+        return ({ cart: updatedCart });
+    }),
+    decreaseQuantity: (id) => set((state) => {
+        const product = state.cart.find((item) => item.id === id);
+        if (!product) {
+            showToast("No product found.");
+        };
+        if (product["quantity"] === 1) {
+            state.removeFromCart(id);
+        }
+        product["quantity"] -= 1;
+        const updatedCart = [...state.cart, product];
+        console.log(updatedCart);
+        return ({ cart: updatedCart });
+    }),
     clearCart: () =>
         set(() => ({
             cart: [],
